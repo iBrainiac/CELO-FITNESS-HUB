3963bab155f8c33a48190b9cb482d1efe2c86e60
iBrainiac <franqhazard@gmail.com>
2022-11-28 07:43:33 -0800

    updated smart contract functions

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -1,3 +1,5 @@
+
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
@@ -14,39 +16,53 @@ interface IERC20Token {
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract Marketplace {
+contract  MarketPlace {
 
-    uint internal productsLength = 0;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint private productsLength = 0;
+    address private cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Product {
         address payable owner;
         string name;
         string image;
         string description;
-        string location;
         uint price;
         uint sold;
+        uint stock;
     }
 
-    mapping (uint => Product) internal products;
+    mapping (uint => Product) private products;
+    mapping (address => uint) private bonus;
 
+    // modifier to check if caller is the owner of product
+    modifier onlyProductOwner(uint _index){
+        require(products[_index].owner == msg.sender, "Unauthorized");
+        _;
+    }
+
+    /**
+        * @dev allow users to create a product on the platform
+        * @notice input data needs to contain only valid values
+     */
     function writeProduct(
-        string memory _name,
-        string memory _image,
-        string memory _description, 
-        string memory _location, 
-        uint _price
+        string calldata _name,
+        string calldata _image,
+        string calldata _description, 
+        uint _price,
+        uint _stock
     ) public {
+        require(bytes(_name).length > 0, "Empty name");
+        require(bytes(_image).length > 0, "Empty image");
+        require(bytes(_description).length > 0, "Empty description");
         uint _sold = 0;
         products[productsLength] = Product(
             payable(msg.sender),
             _name,
             _image,
             _description,
-            _location,
             _price,
-            _sold
+            _sold,
+            _stock
         );
         productsLength++;
     }
@@ -55,9 +71,9 @@ contract Marketplace {
         address payable,
         string memory, 
         string memory, 
-        string memory, 
-        string memory, 
+        string memory,
         uint, 
+        uint,
         uint
     ) {
         return (
@@ -65,25 +81,63 @@ contract Marketplace {
             products[_index].name, 
             products[_index].image, 
             products[_index].description, 
-            products[_index].location, 
             products[_index].price,
-            products[_index].sold
+            products[_index].sold,
+            products[_index].stock
         );
     }
-    
-    function buyProduct(uint _index) public payable  {
+
+    /**
+        * @dev allow users to buy a product listed on the platform, awards Royalt points for every cUSD spent, and give 0.1 cUSD discount for every royaly points spent.
+        * @param _amount is the number of products to buy
+        * @notice Transaction will revert if there are not enough quantity of product in stock to fulfill the order
+     */
+    function buyProduct(uint _index, uint _amount, uint _royaltySpent) public payable  {
+        Product storage currentProduct = products[_index];
+        uint totalAmout = (currentProduct.price * _amount) - (_royaltySpent * 100000000000000000);
+        require(_amount > 0, "You must buy at least one product");
+        require( royalty[msg.sender] >=  _royaltySpent, "Not enought royalties");
+        require(currentProduct.stock >= _amount, "Not enough in stock");
+        require(currentProduct.owner != msg.sender, "You can't buy your products");
         require(
           IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
-            products[_index].owner,
-            products[_index].price
+            currentProduct.owner,
+            totalAmout
           ),
           "Transfer failed."
         );
-        products[_index].sold++;
+        uint newRoyaltyPoints = royalty[msg.sender] + (totalAmout / 1000000000000000000) - _royaltySpent;
+        uint newSoldAmount = currentProduct.sold + _amount;
+        uint newStockAmount = currentProduct.stock - _amount;
+        royalty[msg.sender] = newRoyaltyPoints;
+        currentProduct.sold = newSoldAmount;
+        currentProduct.stock = newStockAmount;
     }
     
     function getProductsLength() public view returns (uint) {
         return (productsLength);
+    }
+
+    /**
+        * @dev allow products' owners to update their stock value
+     */
+    function updateStock(uint _index, uint _stock) public onlyProductOwner(_index) {
+        products[_index].stock = _stock;
+    }
+
+    
+    /**
+        * @dev allow products' owners to update the price of their products
+     */
+    function updatePrice(uint _index, uint _price) public onlyProductOwner(_index) {
+        products[_index].price = _price;
+    }
+
+    /**
+        * @dev allow users to get the amount of royalty points they have
+     */
+    function getRoyaltyPoints() public view returns(uint) {
+        return royalty[msg.sender];
     }
 }
